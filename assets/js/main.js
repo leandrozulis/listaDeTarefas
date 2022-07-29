feb4e337ba18585862d6d2ec64af6ca76c935459
leandrozulis <leandro_zulli@hotmail.com>
2022-07-29 17:06:16 -0300

    Projeto realizado com o intuito de treinar a logica de programação e a manipulação de dados

---
+++ assets/js/main.js
@@ -0,0 +1,77 @@
+const inputTarefa = document.querySelector('.input-tarefa');
+const btnTarefa = document.querySelector('.btn-tarefa');
+const tarefas = document.querySelector('.tarefas');
+
+function criaLi() {
+  const li = document.createElement('li');
+  return li;
+}
+
+function limpaInput() {
+  inputTarefa.value = '';
+  inputTarefa.focus();
+}
+
+function criaBotaoApagar(li) {
+  li.innerText += ' ';
+  const botaoApagar = document.createElement('button');
+  botaoApagar.innerText = 'Apagar';
+  botaoApagar.setAttribute('class', 'apagar');
+  li.appendChild(botaoApagar);
+}
+
+function criaTarefa(texto) {
+  const li = criaLi();
+  li.innerText = texto;
+  tarefas.appendChild(li);
+  criaBotaoApagar(li);
+  salvarTarefas();
+}
+
+function salvarTarefas() {
+  const liTarefas = tarefas.querySelectorAll('li');
+  const listaDeTarefas = [];
+
+  for (let tarefa of liTarefas) {
+    let tarefaTexto = tarefa.innerText;
+    tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+    listaDeTarefas.push(tarefaTexto);
+  }
+
+  const tarefasJSON = JSON.stringify(listaDeTarefas);
+  localStorage.setItem('tarefas', tarefasJSON);
+}
+
+function adicionaTarefasSalvas() {
+  const tarefas = localStorage.getItem('tarefas');
+  const listaDeTarefas = JSON.parse(tarefas);
+
+  for (let tarefa in listaDeTarefas) {
+    criaTarefa(listaDeTarefas[tarefa]);
+  }
+}
+
+btnTarefa.addEventListener('click', function () {
+  if (!inputTarefa.value) return;
+  criaTarefa(inputTarefa.value);
+  limpaInput();
+});
+
+inputTarefa.addEventListener('keypress', function (e) {
+  if (e.keyCode === 13) {
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
+    limpaInput();
+  }
+});
+
+document.addEventListener('click', function (e) {
+  const el = e.target;
+
+  if (el.classList.contains('apagar')) {
+    el.parentElement.remove();
+    salvarTarefas();
+  }
+});
+
+adicionaTarefasSalvas();
